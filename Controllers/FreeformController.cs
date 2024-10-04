e08145aebd6129028b902c0c51ccc31014e634fb
Jacob Morehouse <jacob.morehouse@gmail.com>
2024-10-03 20:43:10 -0700

    Some tinkering with a View and Controller with no model attached, and starting to work with raw SQL in EF.

---
+++ Controllers/FreeformController.cs
@@ -0,0 +1,31 @@
+﻿using JakeDemoSite1.Data;
+using JakeDemoSite1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace JakeDemoSite1.Controllers
+{
+	public class FreeformController : Controller
+	{
+		private readonly DatabaseContext _context;
+
+		public FreeformController(DatabaseContext context)
+		{
+			_context = context;
+		}
+
+		public async  Task<IActionResult> Index()
+		{
+
+			var x = _context.Employees.First();
+
+			List<String> nEmployees = _context.Database.SqlQuery<string>(FormattableStringFactory.Create("select firstname || ' ' || lastname from Employees where lower(firstName) like '%n%'; ")).ToList();
+
+			ViewBag.firstEmployee = x;
+			ViewBag.nEmployees = nEmployees;
+
+			return View();
+		}
+	}
+}
